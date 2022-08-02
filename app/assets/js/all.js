ac6a9ee767092f66814b463d2c501a30b17a28d0
weizhetseng <weizhetseng@gmail.com>
2022-08-03 00:37:04 +0800

    Update 2022-08-02T16:36:25.431Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const navbar = document.querySelector('.mobile-show');
-
-mobileMenu.addEventListener('click',function(){
-  navbar.classList.toggle('open')
-})
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var mobileMenu = document.querySelector('.mobile-menu');
+var navbar = document.querySelector('.mobile-show');
+mobileMenu.addEventListener('click', function () {
+  navbar.classList.toggle('open');
+});
+//# sourceMappingURL=all.js.map
