389ec34f9535e90985175cad3ee7ef9d735467e6
weizhetseng <weizhetseng@gmail.com>
2022-08-05 12:38:01 +0800

    更改jquery

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,13 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const navbar = document.querySelector('.mobile-show');
+// const mobileMenu = document.querySelector('.mobile-menu');
+// const navbar = document.querySelector('.mobile-show');
 
-mobileMenu.addEventListener('click',function(){
-  navbar.classList.toggle('open')
+// mobileMenu.addEventListener('click',function(){
+//   navbar.classList.toggle('open')
+// })
+
+
+$('.mobile-menu').click(function(e){
+  e.preventDefault();
+
+  $('.navbar').toggleClass('open');
 })
