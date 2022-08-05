43cc6868522d853047721d565d8bf4e618c15bf8
weizhetseng <weizhetseng@gmail.com>
2022-08-05 12:38:45 +0800

    Update 2022-08-05T04:38:42.918Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,12 @@
 "use strict";
 
-var mobileMenu = document.querySelector('.mobile-menu');
-var navbar = document.querySelector('.mobile-show');
-mobileMenu.addEventListener('click', function () {
-  navbar.classList.toggle('open');
+// const mobileMenu = document.querySelector('.mobile-menu');
+// const navbar = document.querySelector('.mobile-show');
+// mobileMenu.addEventListener('click',function(){
+//   navbar.classList.toggle('open')
+// })
+$('.mobile-menu').click(function (e) {
+  e.preventDefault();
+  $('.navbar').toggleClass('open');
 });
 //# sourceMappingURL=all.js.map
